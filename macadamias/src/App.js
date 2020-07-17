16ad2936b16aa1355d2e5ff928c1cf83a3fdd98d
Adanieljohnson91 <dan.johnson1022@gmail.com>
2020-07-17 10:43:19 -0500

    HOC added

--- macadamias/src/App.js
+++ macadamias/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Login from "./components/Login/Login";
 import NavBar from './components/navbar/NavBar';
 import { useState } from "react";
+import ApplicationViews from './components/Views/ApplicationViews';
+
 
 function App() {
   const [loggedIn, setLogin] = useState(false)
   return (
-    <div className="App">
-   {loggedIn ? <NavBar/> :
-     <Login setLogin={setLogin}/>}
-    </div>
+    <>
+    <NavBar />
+    <ApplicationViews/>
+    
+    </>
   );
 }
 
