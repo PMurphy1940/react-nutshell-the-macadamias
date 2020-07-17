f9ad7d2d11ad658334cccef27baf4d0baa2381ed
Patrick Murphy <engine_ear@mac.com>
2020-07-16 22:42:41 -0500

    initial setup of news articles component

---
+++ macadamias/src/components/News/NewsList.js
@@ -0,0 +1,14 @@
+import react, { useState, useEffect } from 'react'
+
+const NewsFeed = (props) => {
+const [news, setNews] = useState([])
+
+const getUserAndFriendNews = () => {
+    return 
+}
+
+
+
+
+
+}
