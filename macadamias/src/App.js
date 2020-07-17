b59189c3b1a4f3ca4862b60cc703025c403b0ada
davidbrucetn <severian37@gmail.com>
2020-07-17 11:40:09 -0500

    merging from master

--- macadamias/src/App.js
+++ macadamias/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "./App.css"
+import "./App.css"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import NavBar from './components/navbar/NavBar';
 import ApplicationViews from './components/Views/ApplicationViews';
-import { Card, CardBody, CardText, CardTitle} from "reactstrap"
 
 
 function App() {
@@ -16,17 +15,8 @@ function App() {
             <div className="App-header">
               <Header />
             </div>
-        <div className="div__main">
-            
-                <NavBar />
-                <h3>Messages</h3>
-                <Card>
-                  <CardTitle>Message from DB</CardTitle>
-                  <CardBody>
-                    <CardText>blah blah covid-19 blah</CardText>
-                  </CardBody>
-                </Card>
-
+            <div className="div__main">
+            <NavBar />
 
             <div className="div__appviews">
               <ApplicationViews />
