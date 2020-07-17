6e8914643afc96e3cea6da4ffffd93ef17bc09b7
davidbrucetn <severian37@gmail.com>
2020-07-17 11:36:38 -0500

    updates from master

--- macadamias/src/App.js
+++ macadamias/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css"
+// import "./App.css"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import NavBar from './components/navbar/NavBar';
@@ -17,8 +17,7 @@ function App() {
               <Header />
             </div>
         <div className="div__main">
-
-            <div className="div__navbar">
+            
                 <NavBar />
                 <h3>Messages</h3>
                 <Card>
@@ -27,8 +26,8 @@ function App() {
                     <CardText>blah blah covid-19 blah</CardText>
                   </CardBody>
                 </Card>
-            </div>
-          
+
+
             <div className="div__appviews">
               <ApplicationViews />
               <p>Items</p>
